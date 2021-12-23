b88a0d7b0eb0f16c4dde23f66fe2f029ff7ccd09
Leukino <miquelqg99@gmail.com>
2021-12-23 16:40:06 +0100

    3d gamekit lite

---
+++ Data Analysis assignment 2/Assets/EventHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Gamekit3D;
+using Gamekit3D.Message;
+using static Gamekit3D.Damageable;
+
+
+public class EventHandler : MonoBehaviour, IMessageReceiver
+{
+    // Start is called before the first frame update
+    public void OnReceiveMessage(MessageType type, object sender, object msg)
+    {
+        switch (type) 
+        {
+            case MessageType.DAMAGED:
+                onDamageRecieved((Damageable)sender, (DamageMessage)msg);
+                break;
+        }
+    }
+
+    private void onDamageRecieved(Damageable sender, DamageMessage msg)
+    {
+        Debug.Log("Registered Damaged: " + msg.damager.name + "\nAmount: " + msg.amount + "\nPosition: " + msg.damageSource);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
